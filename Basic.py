a278df08f6bec832784857fd6b06a084fdd94be6
anupo <anupomr@yahoo.com>
2020-04-16 08:29:15 -0400

    Nested loop is implemented to display big F

--- Basic.py
+++ Basic.py
@@ -9,8 +9,16 @@ for item in range(1, 10, 2):  # 1 = starter (opt), 10 = range (req), 2 = step (o
     print(item)
 
 # Add item in loop
-price = [10, 20, 30]
+price = [10, 20, 30]  # imaginary shopping cart
 total = 0
 for item in price:
     total += item
 print(f" Total price ${total}")
+
+# Nested loop
+numbers = [5, 2, 5, 2, 2]  # to display big F
+for x in numbers:
+    display = ""
+    for item in range(x):
+        display += "X"
+    print(display)
