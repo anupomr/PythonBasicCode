403880f1900001dc22b09c4e1e936ec804c04140
anupo <anupomr@yahoo.com>
2020-04-16 23:05:27 -0400

    2 Dimensional Lists

--- Basic.py
+++ Basic.py
@@ -38,3 +38,16 @@ for number in numbers:
     if number > large:
         large = number
 print(large)
+
+
+# 2 Dimensional Lists
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+print(matrix[0])
+matrix[0][2] = 21
+print(matrix[0][2])
+for row in matrix:
+    print(row)
